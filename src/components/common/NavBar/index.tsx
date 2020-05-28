1aee1c653f493427f8d3819474318e8aedc4c2d0
ddongule <mnk918p@gmail.com>
2020-05-28 10:33:24 +0900

    [ADD] navbar components

---
+++ src/components/common/NavBar/index.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+const NavBar: React.FC = () => {
+  return (
+    <div className="nav">
+      <ul>
+        <Link to="#"></Link>
+      </ul>
+    </div>
+  )
+}
+
+export default NavBar
---
+++ src/components/common/NavItem/index.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+
+const NavItem: React.FC = () => {
+  return (
+    <li>
+      <Link to="#"></Link>
+    </li>
+  )
+}
+
+export default NavItem
